4fc7dfe56f3ea8d9e860ead5408562e39b4cfc3c
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-04-14 18:45:36 +0300

    add proxy to specify route add react-router-dom create component add routes in app component add linktags in index.html

---
+++ client/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <p>DASHBOARD</p>
+      <ul>
+        <li><a href='#'>Report</a></li>
+        <li><a href='#'>SALES REPORT</a></li>
+        <li><a href='#'>PRODUCTS</a></li>
+        <li><a href='#'>INVENTORY</a></li>
+        <li><a href='#'>EMPLOYEES</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Home
