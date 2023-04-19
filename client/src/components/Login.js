363bcefee8452f7e820e2e08d23f68d2d218d4c5
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-04-19 12:05:50 +0300

    styled login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -8,8 +8,8 @@ const Login = () => {
 
   const [error, setError] = useState(null);
   const [notify, setNotify] = useState(false);
-  const [username, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
 
   // // const [credentials, setCredentials] = useState({
   //   username: "",
@@ -33,7 +33,7 @@ const Login = () => {
         Accept: "application/json",
         Authorization: localStorage.token,
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ email, password }),
     }).then((res) => {
       if (res.ok) {
         res.json().then((user) => {
@@ -57,69 +57,85 @@ const Login = () => {
     navigate("/dashboard");
   }
   return (
-    <div className="container-login">
-      <div className="screen">
-        <div className="screen__content">
-          {notify ? <p className="alert-text">Login Successful</p> : null}
-          <form className="login" onSubmit={handleSubmit}>
-            <div className="login__field">
-              <i className="login__icon fas fa-user"></i>
-              {error ? <p className="alert-text">{error.errors}</p> : null}
-              <input
-                type="text"
-                name="username"
-                className="login__input"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUserName(e.target.value)}
-              />
-            </div>
-            <div className="login__field">
-              <i className="login__icon fas fa-lock"></i>
-              <input
-                type="password"
-                className="login__input"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-           
-            <button className="button login__submit">
-              <span className="button__text">Log In Now</span>
-              <i className="button__icon fas fa-chevron-right"></i>
-            </button>
-              {/* <div className="button__text">
-             <button  type="submit" className="button login__submit" onClick={handleLogout}>LOGOUT
-             <i className="button__icon fas fa-chevron-right"></i>
-             </button> 
-             </div> */}
-             <div className="button__text">
-             <NavLink  className="button login__submit" to='/'>LOGOUT
-             <i className="button__icon fas fa-chevron-right"></i>
-             </NavLink> 
-             </div> 
-            <NavLink to="/sign-up" className="gotosignup__text">
-              Sign Up
-            </NavLink>
-          </form>
+
+    <div class="form-box">
+    <form class="form" onSubmit={handleSubmit}>
+        <span class="title"> User Login</span>
+        <span class="subtitle">Remember me?</span>
+        <div class="form-container">
+          <input type="email" class="input" placeholder="Email" id='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <input type="password" class="input" placeholder="Password" id='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
         </div>
-        {/* <div class="social-login">
-            <h3>log in via</h3>
-            <div class="social-icons">
-              <p className="social-login__icon fab fa-instagram"></p>
-              <p className="social-login__icon fab fa-facebook"></p>
-              <p className="social-login__icon fab fa-twitter"></p>
-            </div>
-           </div>  */}
-        <div className="screen__background">
-          <span className="screen__background__shape screen__background__shape4"></span>
-          <span className="screen__background__shape screen__background__shape3"></span>
-          <span className="screen__background__shape screen__background__shape2"></span>
-          <span className="screen__background__shape screen__background__shape1"></span>
-        </div>
-      </div>
+        <button>Login</button>
+      
+    
+        </form>
+        
+    
     </div>
+    // <div className="container-login">
+    //   <div className="screen">
+    //     <div className="screen__content">
+    //       {notify ? <p className="alert-text">Login Successful</p> : null}
+    //       <form className="login" onSubmit={handleSubmit}>
+    //         <div className="login__field">
+    //           <i className="login__icon fas fa-user"></i>
+    //           {error ? <p className="alert-text">{error.errors}</p> : null}
+    //           <input
+    //             type="text"
+    //             name="username"
+    //             className="login__input"
+    //             placeholder="Username"
+    //             value={username}
+    //             onChange={(e) => setUserName(e.target.value)}
+    //           />
+    //         </div>
+    //         <div className="login__field">
+    //           <i className="login__icon fas fa-lock"></i>
+    //           <input
+    //             type="password"
+    //             className="login__input"
+    //             placeholder="Password"
+    //             value={password}
+    //             onChange={(e) => setPassword(e.target.value)}
+    //           />
+    //         </div>
+           
+    //         <button className="button login__submit">
+    //           <span className="button__text">Log In Now</span>
+    //           <i className="button__icon fas fa-chevron-right"></i>
+    //         </button>
+    //           {/* <div className="button__text">
+    //          <button  type="submit" className="button login__submit" onClick={handleLogout}>LOGOUT
+    //          <i className="button__icon fas fa-chevron-right"></i>
+    //          </button> 
+    //          </div> */}
+    //          <div className="button__text">
+    //          <NavLink  className="button login__submit" to='/'>LOGOUT
+    //          <i className="button__icon fas fa-chevron-right"></i>
+    //          </NavLink> 
+    //          </div> 
+    //         <NavLink to="/sign-up" className="gotosignup__text">
+    //           Sign Up
+    //         </NavLink>
+    //       </form>
+    //     </div>
+    //     {/* <div class="social-login">
+    //         <h3>log in via</h3>
+    //         <div class="social-icons">
+    //           <p className="social-login__icon fab fa-instagram"></p>
+    //           <p className="social-login__icon fab fa-facebook"></p>
+    //           <p className="social-login__icon fab fa-twitter"></p>
+    //         </div>
+    //        </div>  */}
+    //     <div className="screen__background">
+    //       <span className="screen__background__shape screen__background__shape4"></span>
+    //       <span className="screen__background__shape screen__background__shape3"></span>
+    //       <span className="screen__background__shape screen__background__shape2"></span>
+    //       <span className="screen__background__shape screen__background__shape1"></span>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
