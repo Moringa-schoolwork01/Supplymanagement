396a4607af7f7a1f715fb58ab0ee85ac188a261f
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-04-19 10:13:41 +0300

    add password attribute in signin

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -3,43 +3,41 @@ import React, { useState } from 'react'
 
 function Signin(onLogin) {
 
-    const [name, setName] = useState('')
 const [email, setEmail] = useState('')
 const [password, setPassword] = useState('')
 
 
 const handleSubmit = (e) => {
     e.preventDefault()
-
-    fetch("/", {
+    console.log("bombastic side eye")
+    fetch("/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email }),
+      body: JSON.stringify({ email, password }),
     })
-      .then((r) => r.json())
-      .then((user) => onLogin(user));
+      .then((r) => r.json(onLogin))
+      // .then((user) => onLogin(user));
 }
 
 
   return (
     <div class="form-box">
 <form class="form" onSubmit={handleSubmit}>
-    <span class="title"> User Login</span>
-    <span class="subtitle">Remember Me?</span>
+    <span class="title"> register new user</span>
+    <span class="subtitle">Hey am new</span>
     <div class="form-container">
-      <input type="text" class="input" placeholder="Full Name"id='name' value={name} onChange={(e) => setName(e.target.value)}/>
 			<input type="email" class="input" placeholder="Email" id='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
 			<input type="password" class="input" placeholder="Password" id='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
     </div>
-    <button>Login</button>
+    <button>SignUp</button>
   
 
     </form>
     
 <div class="form-section">
-  <p>Have an account? <a href="#">Sign in</a> </p>
+  <p>Have an account? <a href="login">Sign in</a> </p>
 </div>
 </div>
   )
