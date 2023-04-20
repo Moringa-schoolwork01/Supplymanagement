6efaccb522967b94e57e285a044125fbfbc60403
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-04-20 16:47:19 +0300

    styled login components to add validations

--- db/migrate/20230418200609_create_users.rb
+++ db/migrate/20230418200609_create_users.rb
@@ -1,5 +1,7 @@
 class CreateUser < ActiveRecord::Migration[7.0]
   def change
+    drop_table :users
+    
     create_table :users do |t|
       t.string :email
       t.string :password_digest
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_19_102828) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_120110) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.integer "contact"
