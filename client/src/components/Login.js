34c9a8c5a1e1dc4c8576aa0a617b05b13b06a2e4
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-05-02 09:34:34 +0300

    style dthe sign in components

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,96 +1,204 @@
+import React, { Fragment, useState } from "react";
+import {Row,Col, Avatar, Card, Form, Input, Button, Typography, Alert, Spin } from 'antd';
 
-import React, { useState } from "react";
-import "../css/login.css";
+// import "../css/login.css";
 import { useNavigate } from 'react-router-dom';
+import { EditOutlined, EllipsisOutlined, SettingOutlined,UserOutlined, LockOutlined  } from '@ant-design/icons';
+// import { Form, Input, Button, Typography, Alert, Spin } from 'antd';
+// import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
-
+const { Meta } = Card;
+const { Title } = Typography;
 
 function Login () {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('')
-const [password, setPassword] = useState('')
-const [isLoggedIn, setIsLoggedIn] = useState(false);
-const [errorMessage, setErrorMessage] = useState('');
-const [error, setError] = useState('');
-const [loading, setLoading] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false)
  
-// const handleLogin = () => {
-//   navigate('/')
-// }
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  
-      console.log("ni kunoma mazee")
-      if(email.length===0||password.length===0){
-        setError(true)
-      }
-      const newData = {
-        email: email,
-        password: password
-      }
-      //Console the new data creted
-      console.log(newData)
-      setLoading(true) //Set loading before sending API request
-      //Post newuser with credentials
-      fetch('/login', {
-        method: 'POST',
-        body: JSON.stringify(newData),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-      .then((r) => r.json())
-      .then((newuser) =>{
-        console.log(newuser)
-        // save the token to localStorage for future access
-       localStorage.setItem("jwt", newuser.jwt);
-       setLoading(false) //stop loading
-
-       //shows alert with user details
-       if (newuser.user) {
-        setIsLoggedIn(true);
-        alert(`Login successful! Welcome, ${newuser.user}!`);
-        navigate('/home')
-      } else {
-        setErrorMessage('User does not exist.');
-        alert('Login failed. User does not exist.')
-      }
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+    setLoading(true);
+    //Post newuser with credentials
+    fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
     })
-    .catch(error => {
-      console.error('Error:', error);
-      setErrorMessage('An error occurred. Please try again later.');
-      setLoading(false) //stop loading
-    });
-      
-     
+    .then((r) => r.json())
+    .then((newuser) =>{
+      console.log(newuser)
+      // save the token to localStorage for future access
+     localStorage.setItem("jwt", newuser.jwt);
+     setLoading(false) //stop loading
 
-     
-  }
+     //shows alert with user details
+     if (newuser.user) {
+      setIsLoggedIn(true);
+      alert(`Login successful! Welcome, ${newuser.user}!`);
+      navigate('/home')
+    } else {
+      setErrorMessage('User does not exist.');
+      alert('Login failed. User does not exist.')
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    setErrorMessage('An error occurred. Please try again later.');
+    setLoading(false) //stop loading
+  });
+  };
+
   return (
-<div className='landingpage'>
+    <>
+    
+    
+    <Card
+    style={{
+      width: 900,
+      top:"20vh",
+      left:'20vw',
+      background: '#fcfefe',
+         }}
+    // cover={
+    //   <img
+    //     alt="example"
+    //     src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+    //   />
+    // }
+    // actions={[
+    //   <SettingOutlined key="setting" />,
+    //   <EditOutlined key="edit" />,
+    //   <EllipsisOutlined key="ellipsis" />,
+    // ]}
+  > 
+  <Row>
+            <Col span={14}> <img src="./reg.png" /></Col>
+             <Col span={6}><Form
+          name="login"
+          onFinish={onFinish}
+          initialValues={{
+            remember: true,
+          }}
+          // style={
+          // {fontSize:'700px',
+          // width:400,
+          // height:1000,
 
-       <div className='landingcontent'>
-          <h2 className='signincont'>Lets Get Started</h2>
-          <h3>Input your credentials here</h3>
-        </div>
-    <div>
-     <form class="formone" onSubmit={handleSubmit}>
-    <span class="title">User Login</span>
-    <label for="email" class="label">Email</label>
-    <input type="email" id="email" name="email" required=""  value={email} onChange={(e) => setEmail(e.target.value)} class="input"/>
-    {error&&email.length<=0?
-     <label className="formlbel">Field cant be empty</label>:""}
-    <label for="password" class="label">Password</label>
-    <input type="password" id="password" name="password" required="" class="input" value={password} onChange={(e) => setPassword(e.target.value)}/>
-    {error&&password.length<=0?
-     <label className="formlbel">Field cant be empty</label>:""}
-    <button type="submit" class="submit">{loading ? <>Loading...</> : <>Login</>}</button>
-  </form>
-    </div>
-    </div>
+          // }
+          // }
+        >
+          <Title  className="title">User Login</Title>
+          <Form.Item
+            name="email"
+            rules={[
+              
+              {
+                required: true,
+                message: 'Please input your email!',
+              },
+            ]}
+            
+          >
+            <Input style= {
+              {
+                padding:15,
+                width:300,
+              }
+            }prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password style= {
+              {
+                padding:15,
+                width:300,
+              }
+            }
+            prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Password" />
+          </Form.Item>
+          {errorMessage && <Alert message={errorMessage} type="error" />}
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="submit" loading={loading}>
+              {loading ? <Spin /> : 'Log in'}
+            </Button>
+          </Form.Item>
+        </Form>
+        </Col>
+        <Col span={4}></Col>
+         </Row> 
+    {/* <Meta
+      avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />}
+      title="Card title"
+      description="This is the description"
+    /> */}
+  </Card>
+    </>
+  
+    // <div className='landingpage'>
+    //   <div className='landingcontent'>
+    //     <Title level={2} className='signincont'>Welcome back !</Title>
+    //     <Title level={3}>Input your credentials here</Title>
+    //   </div>
+    //   <div>
+    //        <img className='loginimage' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRecroq9Fmqao54FgAtVCqFRC-014ctCH4ww&usqp=CAU' alt='img'></img>
+    //     </div>
+      
+    //   <div>
+    //     <Form
+    //       name="login"
+    //       onFinish={onFinish}
+    //       initialValues={{
+    //         remember: true,
+    //       }}
+    //     >
+    //       <Title level={4} className="title">User Login</Title>
+    //       <Form.Item
+    //         name="email"
+    //         rules={[
+    //           {
+    //             required: true,
+    //             message: 'Please input your email!',
+    //           },
+    //         ]}
+    //       >
+    //         <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+    //       </Form.Item>
+    //       <Form.Item
+    //         name="password"
+    //         rules={[
+    //           {
+    //             required: true,
+    //             message: 'Please input your password!',
+    //           },
+    //         ]}
+    //       >
+    //         <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Password" />
+    //       </Form.Item>
+    //       {errorMessage && <Alert message={errorMessage} type="error" />}
+    //       <Form.Item>
+    //         <Button type="primary" htmlType="submit" className="submit" loading={loading}>
+    //           {loading ? <Spin /> : 'Log in'}
+    //         </Button>
+    //       </Form.Item>
+    //     </Form>
+        
+    //   </div>
+     
 
+    // </div>
+
+    
+    
   );
 };
 
--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,25 +1,31 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { Fragment, useState } from "react";
+import {Row,Col, Avatar, Card, Form, Input, Button, Typography, Alert, Spin } from 'antd';
 
+// import "../css/login.css";
+import { useNavigate } from 'react-router-dom';
+import { EditOutlined, EllipsisOutlined, SettingOutlined,UserOutlined, LockOutlined  } from '@ant-design/icons';
+// import { Form, Input, Button, Typography, Alert, Spin } from 'antd';
+// import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+const { Meta } = Card;
+const { Title } = Typography;
 
 function Signin() {
   const navigate = useNavigate();
-const [email, setEmail] = useState('')
-const [password, setPassword] = useState('')
-const [confirmpassword, setConfirmPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmpassword, setConfirmPassword] = useState('');
 
-
-// Register new user
-const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log("bombastic side eye")
+  // Register new user
+  const handleSubmit = (values) => {
+    console.log("bombastic side eye");
 
     const newData ={
-      email: email,
-      password: password,
-      password_confirmation: password
-    }
-   console.log (newData)
+      email: values.email,
+      password: values.password,
+      password_confirmation: values.password_confirmation
+    };
+    console.log (newData);
     fetch("/users", {
       method: "POST",
       headers: {
@@ -29,41 +35,78 @@ const handleSubmit = (e) => {
     })
       .then((r) => r.json())
       .then((newme) => {
-        alert(`${newme.user.email} Registration successfuly!`)
-        navigate('/login')
-        console.log(newme)
-      })
-      // .then((user) => onLogin(user));
-}
-
+        alert(`${newme.email} Registration successfuly!`);
+        navigate('/login');
+      });
+  };
 
   return (
-<div className='landingpage'>
-      <div className='landingcontent'>
-          <h2 className='signincont'>Get A New Account</h2>
-          <h3>Register a new account and get started</h3>
-        </div>
-    <div>
-      <form class="formone" onSubmit={handleSubmit}>
-    <span class="title">Register New Account</span>
-    <label for="email" class="label">Email</label>
-    <input type="email" id="email" name="email" required=""  value={email} onChange={(e) => setEmail(e.target.value)} class="input"/>
-    <label for="password" class="label">Password</label>
-    <input type="password" id="password" name="password" required="" class="input" value={password} onChange={(e) => setPassword(e.target.value)}/>
-    <label for="password" class="label">Password Confirmation</label>
-    <input type="password" id="password_confirmation" name="password_confirmation" required="" class="input" value={confirmpassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
-    <button type="submit" class="submit" >Register</button>
-    <div class="form-section">
-  <p>Have an account? <a href="login">login</a> </p>
- </div>
-  </form>
+    <>
+    
+    
+    <Card
+    style={{
+      width: 950,
+      height:500,
+      top:"20vh",
+      left:'30vw',
+      background: '#fcfefe',
+            // background: 'radial-gradient(328px at 2.9% 15%, rgb(191, 224, 251) 0%, rgb(232, 233, 251) 25.8%, rgb(252, 239, 250) 50.8%, rgb(234, 251, 251) 77.6%, rgb(240, 251, 244) 100.7%)',
+    }}
+    // cover={
+    //   <img
+    //     alt="example"
+    //     src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+    //   />
+    // }
+    // actions={[
+    //   <SettingOutlined key="setting" />,
+    //   <EditOutlined key="edit" />,
+    //   <EllipsisOutlined key="ellipsis" />,
+    // ]}
+  > 
+  <Row>
+            <Col span={15}> <img src='./lg.png'  /></Col>
+             <Col span={5}><Form className="formmone" onFinish={handleSubmit}>
+          <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please input your email!' }]}>
+            <Input style= {
+              {
+                padding:15,
+                width:300,
+              }} value={email} onChange={(e) => setEmail(e.target.value)} />
+          </Form.Item>
+          <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+            <Input.Password style= {
+              {
+                padding:15,
+                width:300,
+              }
+            } value={password} onChange={(e) => setPassword(e.target.value)} />
+          </Form.Item>
+          <Form.Item label="Password Confirmation" name="password_confirmation" rules={[{ required: true, message: 'Please confirm your password!' }]}>
+            <Input.Password style= {
+              {
+                padding:15,
+                width:300,
+              }
+            } value={confirmpassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">Register</Button>
+          </Form.Item>
+         
+        </Form>
+        </Col>
+        <Col span={4}></Col>
+         </Row> 
+    {/* <Meta
+      avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />}
+      title="Card title"
+      description="This is the description"
+    /> */}
+  </Card>
+    </>
+    );
+  };
   
-    </div>
-    </div>
-
-  )
-}
-
-export default Signin
-
-
+  export default Signin;
